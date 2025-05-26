838b67cdf561d1f08db7e3b2958baa5b30aae185
Luan Benevides <luan.benevides@boavistatecnologia.com.br>
2025-05-26 17:47:14 -0300

    Ajustando responsividade animações e scroll

--- javascript/iphone-script.js
+++ javascript/iphone-script.js
@@ -4,18 +4,34 @@ const voltar = document.getElementById('back-button');
 const telaBanner = document.getElementById('tela-banner');
 const iphoneBlock = document.getElementById('iphone');
 
-voltar.addEventListener('click', () => {
-    recondicionandoRenderizacao();
-})
+const telaOcultaUm = document.getElementById('oculta-com-iphone-um');
+const telaOcultaDois = document.getElementById('oculta-com-iphone-dois');
+const telaOcultaTres = document.getElementById('oculta-com-iphone-tres');
+const telaOcultaQuatro = document.getElementById('oculta-com-iphone-quatro');
+const telaOcultaCinco = document.getElementById('oculta-com-iphone-cinco');
+const telaOcultaSeis = document.getElementById('oculta-com-iphone-seis');
+
+const video = document.getElementById('demo-video');
+
+
+
 
 demo.addEventListener('click', () => {
     recondicionandoRenderizacao();
 })
 
+video.addEventListener('ended', () => {
+    recondicionandoRenderizacao();
+});
+
+voltar.addEventListener('click', () => {
+    video.pause();
+    recondicionandoRenderizacao();
+});
+
 function recondicionandoRenderizacao() {
 
     if (!telaBanner.classList.contains('all-zindex-one')) {
-        console.log('Vai ocultar o banner');
         telaBanner.classList.add('all-zindex-one');
 
     } else {
@@ -23,14 +39,31 @@ function recondicionandoRenderizacao() {
     }
 
     if (iphoneBlock.classList.contains('ocultado')) {
-        console.log('Identificou que o iphone está oculto')
+        ocultaDemaisTelas();
         iphoneBlock.classList.remove('ocultado');
-        iphoneBlock.classList.add('tela');
+        
         iphoneBlock.classList.add('none-background');
     } else {
         iphoneBlock.classList.add('ocultado');
-
-        iphoneBlock.classList.remove('tela');
+        mostraDemaisTelas()
         iphoneBlock.classList.remove('none-background');
     }
+}
+
+function ocultaDemaisTelas() {
+    telaOcultaUm.classList.add('ocultado');
+    telaOcultaDois.classList.add('ocultado');
+    telaOcultaTres.classList.add('ocultado');
+    telaOcultaQuatro.classList.add('ocultado');
+    telaOcultaCinco.classList.add('ocultado');
+    telaOcultaSeis.classList.add('ocultado');
+}
+
+function mostraDemaisTelas() {
+    telaOcultaUm.classList.remove('ocultado');
+    telaOcultaDois.classList.remove('ocultado');
+    telaOcultaTres.classList.remove('ocultado');
+    telaOcultaQuatro.classList.remove('ocultado');
+    telaOcultaCinco.classList.remove('ocultado');
+    telaOcultaSeis.classList.remove('ocultado');
 }
