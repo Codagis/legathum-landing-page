6c6a4eafada730788e1aca80a91273d6bf18f637
Luan Benevides <luan.benevides@boavistatecnologia.com.br>
2025-05-26 17:47:14 -0300

    Ajustando responsividade animações e scroll

--- javascript/scroll-script.js
+++ javascript/scroll-script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const elementos = document.querySelectorAll('.anima-scroll');
 
-  const observer = new IntersectionObserver((entradas) => {
+  const observer = new IntersectionObserver((entradas, observer) => {
     entradas.forEach((entrada) => {
       if (entrada.isIntersecting) {
         entrada.target.classList.add('visivel');
-      } else {
-        entrada.target.classList.remove('visivel');
+        observer.unobserve(entrada.target);
       }
     });
   }, {
@@ -14,4 +13,22 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   elementos.forEach(el => observer.observe(el));
+});
+
+
+let lastScrollTop = 0;
+const header = document.getElementById("main-header");
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Rolando para baixo
+    header.style.top = "-80px"; // Esconde o header (ajuste se necessário)
+  } else {
+    // Rolando para cima
+    header.style.top = "0";
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Evita valores negativos
 });
